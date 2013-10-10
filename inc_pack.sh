576cc241c7651820dae8268f47dbc57e0f42fc02
WilliamX <william.xue@gmail.com>
2013-10-10 17:11:03 +0800

    + update

--- inc_pack.sh
+++ inc_pack.sh
@@ -1,1 +1,14 @@
-inc_pack.sh
+#!/bin/sh
+# extra the increment files between two commits
+
+if [ $# -ne 2 ]; then
+    echo "usage: selected two commits in SourceTree and run this action again"
+    exit 1
+fi
+
+echo "extra the diff from $2 to $1"
+echo "++++++++++++++++++++++++++++"
+git diff --name-status --diff-filter=AM $2...$1
+echo "++++++++++++++++++++++++++++"
+echo "archive files to `pwd`/update.zip"
+git archive -o update.zip $1 $(git diff --name-only --diff-filter=ACMRT $2...$1)
